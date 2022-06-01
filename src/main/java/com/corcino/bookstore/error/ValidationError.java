06859f17f8ab24a93eba5161441d5571f5c66806
Laion Corcino <laioncorcino@gmail.com>
2022-06-01 02:15:59 -0300

    feat: criando error handler

---
+++ src/main/java/com/corcino/bookstore/error/ValidationError.java
@@ -0,0 +1,12 @@
+package com.corcino.bookstore.error;
+
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+public class ValidationError extends StandardError {
+
+    private String field;
+
+}
